f8e0fc4e438fa6d8a464ff78fc8f329afcb0b708
DeltaMfront <nsfrontmela@gmail.com>
2023-09-12 19:28:28 +0400

    first commit

---
+++ Day 1 Less 1/homework.py
@@ -0,0 +1,12 @@
+name = "me var giorgi "
+surname = "gorgiladze "
+age = 25 
+height = 183.5
+
+print(name)
+print (surname)
+print(age)
+print(height)
+print(name +  surname + " " + str(age) + "clis" + " simagle " +  str(height))
+
+
---
+++ Day 1 Less 1/lesson1.py
@@ -0,0 +1,28 @@
+name ="giorgi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#  "giorgi" არის ცვლადისთვის მნიშვნელობა
+
+surname ="gorgiladze"
+
+#print (name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი 
+
+name= "giorgi" #ეს არის str (string)ტიპის ცვლადი
+age = 22  # ეს არის int (integer) მთელი რიცხვი
+height = 183.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#booLean (bool) ტიპის ცვლადი 
+knows_programming = True 
+is_ugly =False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+isUgly =False # ჯავასქრიპტული Camelcase
+# stringი არის ბრჭყალებში მოქცეული სიმბოლოები 
+#print(name + " " + surname)
+#print (name +age)
+# " " ნიშნავს SPACEს , ადგილის გამოტოვებას 
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
