3bcb457f40c0640919567b16bb154598249af4cb
DeltaMfront <143406922+DeltaMfront@users.noreply.github.com>
2023-10-01 17:10:48 +0400

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,24 @@
+#name = "tom"
+#salery = 75000
+#print("name")
+
+message= "debugging"
+print(message)
+
+
+
+
+balance =20 
+print(balance)
+
+
+name="Giorgi"
+age=25
+height=182.2
+
+print(name +"" + str(age) +" "+str(height))
+
+print("game over")
+
+
+
---
+++ homework.py
@@ -0,0 +1,16 @@
+#costumer name, surname,age.
+
+
+
+
+name=input("what is ur name:")
+surname=input("what is ur surname:")
+age=int(input("what is your age:"))
+
+
+# name="james"
+# surname="mcgregor"
+# age="u will be 50 y old"
+
+age+=25
+print("after 25 you will be  " +str(age))
